157c755dd271fb6d397cb1e5a1dff1da2fb86b82
Vadim <vadim.212@inbo.ru>
2020-12-03 20:54:53 +0600

    Added some prints to the console in lab6/ex1

--- lab6/src/kz/ktu/lab6/ex1/Main.java
+++ lab6/src/kz/ktu/lab6/ex1/Main.java
@@ -7,14 +7,21 @@ public class Main {
 
     public static void main(String[] args) {
         Car car = new Car(new Engine(0.08), List.of(new Wheel(),new Wheel(),new Wheel(),new Wheel()), "car");
+        System.out.println("Марка автомобиля:");
+        car.printModel();
+
         try {
+            System.out.println("Сериализация объекта Car...");
             Car.serialize(car, "car");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         try {
-            Car.deserialize("car").printModel();
+            System.out.println("Десериализация объекта Car...");
+            Car savedCar = Car.deserialize("car");
+            System.out.println("Марка автомобиля:");
+            savedCar.printModel();
         } catch (IOException e) {
             e.printStackTrace();
         }
